a3937bfd3debe57fb062f2f38b93506a1c8d9e4f
VishalSinghShekhawat <vss1664883@gmail.com>
2024-04-13 11:51:47 +0530

    Attempt3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,20 @@ package com.driver;
 import java.util.*;
 
 public class Main {
-  B b1  = new B();
-String s = b1.meth();
+ public class A{
+   public String meth(){
+     return "Invoking method from class A";
+   }
+
+ }
+
+ public class B extends A{
+   @Override
+   public String meth() {
+     return "Method is overridden in Extendend class B";
+   }
+ }
+
+ B b1 = new B();
+ String s = b1.meth();
 }
